e7e3acdf721e1cb4d7da6d5f38ee44a79b490c9d
abie115 <donicczka@gmail.com>
2016-02-23 00:12:17 +0100

    add example OutOfMemoryError

--- src/main/java/com/abiewska/jvm/App.java
+++ src/main/java/com/abiewska/jvm/App.java
@@ -1,13 +1,19 @@
 package com.abiewska.jvm;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+	
+	public static void main(String[] args) {
+
+		List<int[]> lista = new ArrayList<int[]>();
+		try {
+			while (true) {
+				lista.add(new int[5000000]);
+			}
+		} catch (OutOfMemoryError e) {
+			System.out.println(e);
+		}
+	}
 }
