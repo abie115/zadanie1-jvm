3f5d3dbebb54a0a53115cfecf81000d1fbbcdef2
abie115 <donicczka@gmail.com>
2016-02-24 22:22:59 +0100

    add Readme.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# JVM zadanie1
+Program za pomocą pętli while dodaje w nieskończoność dużą tablice typu int do listy. To powoduje wystąpienie wyjątku OutOfMemoryError.
+
+
+Uruchomienie:
+
+```mvn compile exec:java```
--- src/main/java/com/abiewska/jvm/App.java
+++ src/main/java/com/abiewska/jvm/App.java
@@ -4,16 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
-	
+
 	public static void main(String[] args) {
 
 		List<int[]> lista = new ArrayList<int[]>();
-		try {
-			while (true) {
-				lista.add(new int[5000000]);
-			}
-		} catch (OutOfMemoryError e) {
-			System.out.println(e);
+		while (true) {
+			lista.add(new int[5000000]);
 		}
 	}
 }
